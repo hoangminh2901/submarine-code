54997d670fe65cecb80c22060b3844b8e02054bb
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2024-11-29 13:35:42 +0700

    update upload

--- upload.py
+++ upload.py
@@ -1,25 +1,35 @@
 import subprocess
 
-print("Copy index.html to WebPage.h...")
-with open("index.html", "r") as html_file:
-    with open("WebPage.h", "w") as h_file:
-        h_file.write('const char PAGE_MAIN[] PROGMEM = R"=====(')
-        h_file.write(html_file.read())
-        h_file.write(')=====";')
+boards = ["base", "submarine"]
+for board in boards:
+    choice = input(f"Ready to upload {board}? (Enter/n): ")
+    if choice == "n":
+        continue
 
-print("Compiling...")
-compiled = subprocess.run(["arduino-cli", "compile", "--fqbn",
-            "esp32:esp32:uPesy_wroom", "BoatCode.ino"])
-if (compiled.returncode):
-    print("Compilation failed!")
-    exit(1)
+    if board == "base":
+        input(f"Ready to upload {board}?")
+        print(f"Uploading {board}...")
+        print("Copy index.html to WebPage.h...")
+        with open(f"{board}/index.html", "r") as html_file:
+            with open(f"{board}/WebPage.h", "w") as h_file:
+                h_file.write('const char PAGE_MAIN[] PROGMEM = R"=====(')
+                h_file.write(html_file.read())
+                h_file.write(')=====";')
 
-board_list = subprocess.run(["arduino-cli", "board", "list"], capture_output=True)
-board_list = board_list.stdout.decode("utf-8")
-board_list = board_list.split("\n")
-board_list = [line for line in board_list if "serial" in line]
-board_list = [line.split(" ")[0] for line in board_list]
-ESP32_board = board_list[0]
-print("Uploading to " + ESP32_board)
-subprocess.run(["arduino-cli", "upload", "-p", ESP32_board, "--fqbn",
-                "esp32:esp32:uPesy_wroom", "BoatCode.ino"])
+    print("Compiling...")
+    compiled = subprocess.run(["arduino-cli", "compile", "--fqbn",
+                               "esp32:esp32:uPesy_wroom", f"{board}/{board}.ino"])
+    if (compiled.returncode):
+        print("Compilation failed!")
+        exit(1)
+
+    board_list = subprocess.run(
+        ["arduino-cli", "board", "list"], capture_output=True)
+    board_list = board_list.stdout.decode("utf-8")
+    board_list = board_list.split("\n")
+    board_list = [line for line in board_list if "serial" in line]
+    board_list = [line.split(" ")[0] for line in board_list]
+    ESP32_board = board_list[0]
+    print("Uploading to " + ESP32_board)
+    subprocess.run(["arduino-cli", "upload", "-p", ESP32_board, "--fqbn",
+                    "esp32:esp32:uPesy_wroom", f"{board}/{board}.ino"])
